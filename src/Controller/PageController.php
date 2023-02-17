dcaeedf22a210d9ac2f05d7818b028b1514171b7
Jose Luis Olivar <olivarjoseluis9@gmail.com>
2023-02-16 21:58:14 -0500

    Comments views

--- src/Controller/PageController.php
+++ src/Controller/PageController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Entity\Comment;
 use App\Entity\Product;
 use App\Entity\Tag;
 
@@ -13,40 +14,52 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class PageController extends AbstractController
 {
-    #[Route('/', name: 'app_home')]
-    public function home(EntityManagerInterface $entityManager): Response
-    {
-        return $this->render(
-            'page/home.html.twig',
-            [
-                'title' => 'Home',
-                'products' => $entityManager->getRepository(Product::class)->findAll()
-            ]
-        );
-    }
+  #[Route('/', name: 'app_home')]
+  public function home(EntityManagerInterface $entityManager): Response
+  {
+    return $this->render(
+      'page/home.html.twig',
+      [
+        'title' => 'Home',
+        'products' => $entityManager->getRepository(Product::class)->findAll()
+      ]
+    );
+  }
 
-    #[Route('/tag/{id}', name: 'app_tag')]
-    public function tag(Tag $tag): Response
-    {
-        return $this->render(
-            'page/tag.html.twig',
-            [
-                'name' => $tag->getName(),
-                'tag' => $tag
-                //'products' => $tag->getProducts()
-            ]
-        );
-    }
+  #[Route('/tag/{id}', name: 'app_tag')]
+  public function tag(Tag $tag): Response
+  {
+    return $this->render(
+      'page/tag.html.twig',
+      [
+        'name' => $tag->getName(),
+        'tag' => $tag
+        //'products' => $tag->getProducts()
+      ]
+    );
+  }
 
-    #[Route('/product/{id}', name: 'app_product')]
-    public function product(Product $product): Response
-    {
-        return $this->render(
-            'page/product.html.twig',
-            [
-                'title' => $product->getName(),
-                'product' => $product
-            ]
-        );
-    }
+  #[Route('/product/{id}', name: 'app_product')]
+  public function product(Product $product): Response
+  {
+    return $this->render(
+      'page/product.html.twig',
+      [
+        'title' => $product->getName(),
+        'product' => $product
+      ]
+    );
+  }
+
+  #[Route('/comments', name: 'app_comments')]
+  public function comments(EntityManagerInterface $entityManager): Response
+  {
+    return $this->render(
+      'comments/index.html.twig',
+      [
+        'title' => 'Comments',
+        'comments' => $entityManager->getRepository(Comment::class)->findAll()
+      ]
+    );
+  }
 }
